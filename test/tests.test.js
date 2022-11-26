178447e6598776dc0d59f39343d0d536e20833f3
Thomas Parker <91673095+soloth@users.noreply.github.com>
2022-11-25 20:13:40 -0800

    test unauthorized kyc

--- test/tests.test.js
+++ test/tests.test.js
@@ -3,29 +3,69 @@ const PuffinApprovals = artifacts.require("PuffinApprovals");
 
 contract("PuffinKYC Mainnet", async (accounts) => {
     describe("init test token", async () => {
-        it("can add approvers", async () => {
+        it("owner can add approvers", async () => {
             let approvals = await PuffinApprovals.deployed();
             await approvals.allowApprove(accounts[0])
             let isApproved = await approvals.canApprove(accounts[0])
             assert.equal(isApproved, true)
         })
-        it("can approve users", async () => {
+        it("owner can approve users", async () => {
             let approvals = await PuffinApprovals.deployed();
             await approvals.approve(accounts[0])
             let isApproved = await approvals.isApproved(accounts[0])
             assert.equal(isApproved, true)
         })
-        it("can remove users", async () => {
+        it("owner can remove users", async () => {
             let approvals = await PuffinApprovals.deployed();
             await approvals.remove(accounts[0])
             let isApproved = await approvals.isApproved(accounts[0])
             assert.equal(isApproved, false)
         })
-        it("can remove approvers", async () => {
+        it("owner can remove approvers", async () => {
             let approvals = await PuffinApprovals.deployed();
             await approvals.removeApprove(accounts[0])
             let isApproved = await approvals.canApprove(accounts[0])
             assert.equal(isApproved, false)
+        })
+        it("unauthorized user cant add approvers", async () => {
+            let approvals = await PuffinApprovals.deployed();
+            try {
+                await approvals.allowApprove(accounts[0])
+                assert.equal(false, true)
+
+            } catch {
+                assert.equal(true, true)
+            }
+        })
+        it("unauthorized user cant approve users", async () => {
+            let approvals = await PuffinApprovals.deployed();
+            try {
+                await approvals.approve(accounts[0])
+                assert.equal(false, true)
+
+            } catch {
+                assert.equal(true, true)
+            }
+        })
+        it("unauthorized user cant remove users", async () => {
+            let approvals = await PuffinApprovals.deployed();
+            try {
+                await approvals.remove(accounts[0])
+                assert.equal(false, true)
+
+            } catch {
+                assert.equal(true, true)
+            }
+        })
+        it("unauthorized user cant remove approvers", async () => {
+            let approvals = await PuffinApprovals.deployed();
+            try {
+                await approvals.removeApprove(accounts[0])
+                assert.equal(false, true)
+
+            } catch {
+                assert.equal(true, true)
+            }
         })
     })
 });
